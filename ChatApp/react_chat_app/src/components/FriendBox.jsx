ec91cf10a995f672e77e6ddc78d41ac24039a207
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-01-05 19:50:02 -0500

    jmj

---
+++ ChatApp/react_chat_app/src/components/FriendBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FriendBox = (props) => {
+    return (
+        <div className="friendBox">
+            <p>Friends</p>
+            <div className="friendCont">
+            </div>
+
+        </div>
+    );
+}
+
+export default FriendBox;
---
+++ ChatApp/react_chat_app/src/components/Rooms.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rooms = (props) => {
+    return (
+        <div className="roomBox">
+            <p>Rooms</p>
+            <div className="roomCont">
+
+            </div>
+        </div>
+    );
+}
+
+export default Rooms;
