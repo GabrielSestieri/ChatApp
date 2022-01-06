7005cf428d462bd69feaea3ce6c06a50cca710f0
GabrielSestieri <63177325+GabrielSestieri@users.noreply.github.com>
2022-01-05 19:48:51 -0500

    Update App.js

--- ChatApp/react_chat_app/src/App.js
+++ ChatApp/react_chat_app/src/App.js
@@ -2,16 +2,46 @@ import './App.css';
 import React from 'react';
 import Chatfeed from './components/ChatFeed';
 import { BrowserRouter as Router } from "react-router-dom";
-import firebase from 'firebase/app'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import { useAuthState } from 'react-firebase-hooks/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBad9zRHCMMjFN53OW1tBg-fzNpY6ZjnCM",
+  authDomain: "chatapp-e003c.firebaseapp.com",
+  projectId: "chatapp-e003c",
+  storageBucket: "chatapp-e003c.appspot.com",
+  messagingSenderId: "598157875529",
+  appId: "1:598157875529:web:a74c3c4fa61a926cc8e298",
+  measurementId: "G-3CLSQ7D7R6"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <Router>
       <div className="chat-title-container">
-        <div>
-
+        <div className="chat-title">
+          Chat App
         </div>
-      </div>
+        <div className="chat-tophead">
+          <div className="chat-subtitle">
+            by Tuckerman House
+          </div>
+          <div className="chat-nav-links">
+            <nav className="nav">
+              <a href="/">Chat</a>
+              <a href="/">Friends</a>
+              <a href="/">Groups</a>
+              <a href="/">Logout</a>
+            </nav>
+          </div>
+        </div>  
+      </div>  
       <div className="container">
         <div className="roomBox">
           <p>Rooms</p>
