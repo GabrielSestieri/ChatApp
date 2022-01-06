07428b1e179c0d9d4d773bfd2d293faf57381ed8
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-01-06 03:26:51 -0500

    added chat

--- ChatApp/react_chat_app/src/components/Home.jsx
+++ ChatApp/react_chat_app/src/components/Home.jsx
@@ -1,12 +1,82 @@
 import React from 'react'
 import ChatFeed from './ChatFeed'
 import '../home.css'
+import '../chatfeed.css';
 import FriendBox from './FriendBox'
 import Rooms from './Rooms'
 import { useState, useRef, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 
 const Home = (prop) => {
+
+    const auth = firebase.auth();
+    const firestore = firebase.firestore();
+
+    function ChatRoom() {
+        const webhook = useRef();
+        const messagesRef = firestore.collection('messages');
+        const query = messagesRef.orderBy('createdAt');
+        const [messages] = useCollectionData(query, { idField: 'id' });
+        const [formValue, setFormValue] = useState('');
+
+        useEffect(() => {
+            if (webhook.current) {
+                scrollToBottom();
+            }
+        })
+
+        const sendMessage = async (e) => {
+            e.preventDefault();
+
+            const { uid, photoURL } = auth.currentUser;
+
+            await messagesRef.add({
+                text: formValue,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                uid,
+                photoURL
+            })
+
+            setFormValue('');
+            webhook.current.scrollIntoView({ behavior: 'smooth' });
+        }
+
+        const scrollToBottom = () => {
+            webhook.current.scrollIntoView({ behavior: 'smooth' });
+        }
+
+        return (
+            <>
+                <main>
+                    {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+                    <span ref={webhook}></span>
+                </main>
+                <form onSubmit={sendMessage}>
+                    <input value={formValue} placeholder='Send a message...' onChange={(e) => setFormValue(e.target.value)} />
+                    <button type="submit" disabled={!formValue}>Send</button>
+                </form>
+            </>
+        )
+    }
+
+    function ChatMessage(props) {
+        const { text, uid, photoURL } = props.message;
+        const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+        return (<>
+            <div className={`message ${messageClass}`}>
+                <img src={photoURL} />
+                <p>{text}</p>
+            </div>
+
+        </>)
+
+    }
 
     const [currentScreen, setCurrentScreen] = useState(1);
 
@@ -15,7 +85,7 @@ const Home = (prop) => {
             return <Rooms />
         }
         else if (currentScreen == 2) {
-            return <ChatFeed />
+            return <ChatRoom />
         }
         else {
             return <FriendBox />
