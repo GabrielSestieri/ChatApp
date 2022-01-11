f47f15b2c99f54e305c7ada2be5564652a86785c
GabrielSestieri <63177325+GabrielSestieri@users.noreply.github.com>
2022-01-10 21:06:13 -0500

    css

--- ChatApp/react_chat_app/src/components/Forum.jsx
+++ ChatApp/react_chat_app/src/components/Forum.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 const Forum = (props) => {
     return (
         <div className="forumBox">
+            <div className="forumCont">
+                
+            </div>
             
         </div>
     );
